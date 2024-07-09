de954825e35f32a422c74f0908b1c827f2c858ca
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-07-09 15:17:42 +0500

    responsive Home & About

--- src/component/Abouts.jsx
+++ src/component/Abouts.jsx
@@ -4,11 +4,8 @@ import { about } from "../assets";
 
 const Abouts = () => {
   return (
-    <div className="w-full flex flex-wrap items-center justify-center gap-[167px] py-10">
-      <div className=" flex py-10">
-        <img src={about} alt="Profile" className="w-[400px]" />
-      </div>
-      <div className="flex flex-col gap-10 ">
+    <div className="w-full flex flex-wrap items-center justify-between max-[1197px]:justify-center  max-[1137px]:py-5 min-[1137px]:py-10">
+      <div className="flex flex-col gap-10 max-[1137px]:text-center max-[1137px]:items-center ">
         <p className="text-5xl montserrat font-bold max-w-[410px]">
           I love being a developer !
         </p>
@@ -19,7 +16,7 @@ const Abouts = () => {
           into functional and engaging web experiences.
         </p>
 
-        <div className="flex gap-8 flex-wrap">
+        <div className="flex gap-8 flex-wrap justify-center">
           {Projects.map((id, index) => (
             <div className="px-7 py-5 project-bor w-[220px]" key={id.number}>
               <p className="text-4xl montserrat font-bold text-[#ffc107] ">
@@ -30,6 +27,10 @@ const Abouts = () => {
           ))}
         </div>
       </div>
+      <div className=" flex py-10">
+        <img src={about} alt="Profile" className="w-[400px] mx-[53px]" />
+      </div>
+      
     </div>
   );
 };
