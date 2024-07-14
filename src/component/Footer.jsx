6fe54e40f8b7b69e219f529d5041d91bf4b5a63f
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-07-14 15:36:59 +0500

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,12 +3,12 @@ import Socialmedia from '../constant/Socialmedia';
 
 const Footer = () => {
   return (
-    <div className="w-full flex montserrat justify-between flex-wrap items-center text-white  max-[1137px]:py-5 min-[1137px]:py-4">
+    <div className="w-full flex montserrat justify-between flex-wrap items-center text-white gap-10 max-[650px]:justify-center max-[650px]:flex-col max-[1137px]:py-5 min-[1137px]:py-4">
       <div>
         <Socialmedia/>
       </div>
       <div>
-        <p className="text-sm">Made with ❤️ and lots of ☕ from 2024 ©</p>
+        <p className="text-sm text-center">Made with ❤️ and lots of ☕ from 2024 ©</p>
       </div>
     </div>
   )
